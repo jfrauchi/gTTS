f20c1c8a9d843921d9904a6df0e923bd9879086a
John Frauchiger <jfrauchi@its.jnj.com>
2021-11-18 21:32:09 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Text-to-Speech using gTTS
+
+Example script to convert text-to-speech using the gTTS library and Google's TTS API.
+
+## Installation
+
+```bash
+git clone https://github.com/jfrauchi/TTS.git
+```
+
+## Usage
+```bash
+python main.py --help
+
+Usage: main.py [OPTIONS]
+
+  Convert text-to-speech using the gTTS library and Googles TTS API.
+
+Options:
+  -w, --words TEXT     Enter a string you want to convert to an audio file
+  -f, --filename TEXT  Enter file path to text file you want to convert to an
+                       audio file
+  -o, --output TEXT    Choose filname for output file. E.g. 'output.mp3'
+                       [default: my_file.mp3]
+  -l, --lang TEXT      Choose language  [default: en]
+  -t, --tld TEXT       Choose language accent by entering a Google Top Level
+                       Domain. E.g. 'co.uk'  [default: com]
+  --slow BOOLEAN       Only set to True when you want the text to be read more
+                       slowly  [default: False]
+  --help               Show this message and exit.
+```
+
+### String to MP3
+```bash
+python main.py -w "Well, hello there! Nice to meet you." -o "file.mp3"
+python main.py --words "Well, hello there! Nice to meet you." --output "file.mp3"
+```
+### Text file to MP3
+```bash
+python main.py -f "lotsofwords.txt" -o "file.mp3"
+python main.py --filename "lotsofwords.txt" --output "file.mp3"
+```
+### Change the language
+```bash
+python main.py -w "Muy buenos días" -o "file.mp3" -l "es"
+python main.py --words "Muy buenos días" --output "file.mp3" --lang "es"
+```
+### Change the accent
+```bash
+python main.py -w "Well, hello there! Nice to meet you." -o "file.mp3" -l "en" -t "co.in"
+python main.py --words "Well, hello there! Nice to meet you." --output "file.mp3" --lang "en" --tld "co.in"
+```
+
+
---
+++ main.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+"""
+Sample script to translate text to speech
+using the gTTS library and Google's TTS API
+"""
+
+import os
+import sys
+import click
+from gtts import gTTS
+
+
+@click.command()
+@click.option("-w", "--words", help="Enter a string you want to convert to an audio file")
+@click.option("-f", "--filename", help="Enter file path to text file you want to convert to an audio file")
+@click.option("-o", "--output", default="my_file.mp3", 
+              show_default=True, help="Choose filname for output file. E.g. 'output.mp3'")
+@click.option("-l", "--lang", default="en", show_default=True, help="Choose language")
+@click.option("-t", "--tld", default="com", show_default=True, 
+              help="Choose language accent by entering a Google Top Level Domain. E.g. 'co.uk'")
+@click.option("--slow", default=False, show_default=True, 
+              help="Only set to True when you want the text to be read more slowly")
+def text_to_speech(words="", filename="", output="my_file.mp3", lang="en", tld="com", slow=False):
+    """
+    Convert text-to-speech using the gTTS library and Google's TTS API.
+    """
+    if words and filename:
+        print("Please only choose one of the input options, '--words' or '--file', but not both.")
+        sys.exit(1)
+    elif words:
+        convert_to_speech(text=words, output=output, lang=lang, tld=tld, slow=slow)
+        play_audio(output)
+    elif filename:
+        try:
+            with open(filename, "r") as f:
+                text = f.read()
+            convert_to_speech(text=text, output=output, lang=lang, tld=tld, slow=slow)
+            play_audio(output)
+        except FileNotFoundError:
+            print(f"Sorry, the file {filename} does not exist.")
+    else:
+        print("Please choose one of the input options, '--words' or '--file'.")
+        print("Use '--help' to see all options.")
+        sys.exit(1)
+
+
+def convert_to_speech(text, output, lang, tld, slow):
+    """
+    use Google's API to convert the input text to an audio file
+    """
+    audio_created = gTTS(text=text, lang=lang, tld=tld,
+                        slow=slow)
+    audio_created.save(output)
+
+
+def play_audio(audiofile):
+    """
+    check which operating system the script is executed from
+    and the play the audio file using a cli command
+    """
+    if sys.platform == "linux" or sys.platform == "linux2":
+        os.system(f"mpg123 {audiofile}")
+    elif sys.platform == "darwin":
+        os.system(f"afplay {audiofile}")
+    elif sys.platform == "win32":
+        os.system(f"start {audiofile}")
+
+
+if __name__ == "__main__":
+    text_to_speech()
+ 
