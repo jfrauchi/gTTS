626e03d5b20990252ff29cb335a50fa4d5441854
John Frauchiger <jfrauchi@its.jnj.com>
2021-11-19 09:58:35 +0100

    add more comments

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ from gtts import gTTS
 
 
 @click.command()
-@click.option("-w", "--words", help="Enter a string you want to convert to an audio file")
-@click.option("-f", "--filename", help="Enter file path to text file you want to convert to an audio file")
+@click.option("-w", "--words", 
+              help="Enter a string you want to convert to an audio file")
+@click.option("-f", "--filename", 
+              help="Enter file path to text file you want to convert to an audio file")
 @click.option("-o", "--output", default="my_file.mp3", 
               show_default=True, help="Choose filname for output file. E.g. 'output.mp3'")
 @click.option("-l", "--lang", default="en", show_default=True, help="Choose language")
@@ -25,12 +27,16 @@ def text_to_speech(words="", filename="", output="my_file.mp3", lang="en", tld="
     """
     Convert text-to-speech using the gTTS library and Google's TTS API.
     """
+    # first check whether both input options were given. We only want one.
     if words and filename:
         print("Please only choose one of the input options, '--words' or '--file', but not both.")
         sys.exit(1)
+    # if a string was given, send the string as-is to be converted
     elif words:
         convert_to_speech(text=words, output=output, lang=lang, tld=tld, slow=slow)
         play_audio(output)
+    # if a filename was given, open and read the file into a string
+    # then send it to be converted
     elif filename:
         try:
             with open(filename, "r") as f:
@@ -39,6 +45,7 @@ def text_to_speech(words="", filename="", output="my_file.mp3", lang="en", tld="
             play_audio(output)
         except FileNotFoundError:
             print(f"Sorry, the file {filename} does not exist.")
+    # if no input option was given, print a message and exit
     else:
         print("Please choose one of the input options, '--words' or '--file'.")
         print("Use '--help' to see all options.")
